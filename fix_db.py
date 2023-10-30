c0294e6dcc7af2100135ff7f9e3741511ed3bbf2
Jeroen de Vries <jeroen.de.vries@opencloudpartners.nl>
2023-10-30 23:38:33 +0100

    First base of new script. Now coming to db logic.

--- fix_db.py
+++ fix_db.py
@@ -16,33 +16,46 @@ args = parser.parse_args()
 ## Variables
 PS5_IP = args.PS5_IP
 PORT=1337
+files = []
 
-# Directory names
-base_directory = 'tmp'
-sub_directory = 'backup'
+def sort_files(file) :
+	if("CUSA" in file) :
+		files.append("'%s'" % file[-9:])
 
-# Create the base directory ('tmp') if it doesn't exist
-if not os.path.exists(base_directory):
-    os.makedirs(base_directory)
-    print(f"INFO:: Created '{base_directory}' directory")
-else:
-    print(f"INFO:: '{base_directory}' directory already exists")
+# Directory names to create
+dirs_to_create = ['tmp', 'backup']
 
-# Create the subdirectory ('backup') inside the base directory
-backup_directory = os.path.join(base_directory, sub_directory)
-if not os.path.exists(backup_directory):
-    os.makedirs(backup_directory)
-    print(f"INFO:: Created '{sub_directory}' directory inside '{base_directory}'")
-else:
-    print(f"INFO:: '{sub_directory}' directory inside '{base_directory}' already exists")
+# Create directories if they don't exist
+for directory in dirs_to_create:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"INFO:: Created '{directory}' directory")
+    else:
+        print(f"INFO:: '{directory}' directory already exists")
 
-## Get files list from FTP
+# Start FTP connection
 ftp = FTP()
-ftp.connect(PS5_IP, 1337, timeout=30)
+ftp.connect(PS5_IP, PORT, timeout=30)
 ftp.login(user='username', passwd = 'password')
-if(len(files) == 0) :
-        ftp.cwd('/user/app/')
-        ftp.dir(sort_files)
-        print(files)
 
+# List of DB files to copy to temporary folder
+files_to_copy = ['app.db', 'appinfo.db']
+for file_name in files_to_copy:
+    print(f"INFO:: Copying file: {file_name}")
+    try:
+        ftp.cwd('/system_data/priv/mms/')
+        lf = open(dirs_to_create[0] + '/' + file_name, "wb")
+        ftp.retrbinary(f"RETR {file_name}", lf.write)
+        lf.close()
+    except Exception as e:
+        print(f"ERROR:: Error copying {file_name}: {str(e)}")
 
+# Create list of games on the system that are (probably) missing
+## If external, change to /mnt/ext0/user/app or /mnt/ext1/user/app
+if len(files) == 0:
+    ftp.cwd('/user/app/')
+    ftp.dir(sort_files)
+    print("Games found in /user/app:")
+    print(' ')
+    print(files)
+    print(' ')
