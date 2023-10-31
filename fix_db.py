1219c5b175dc4407730f48be6ec4606371aa2983
Jeroen de Vries <jeroen.de.vries@opencloudpartners.nl>
2023-10-31 11:04:30 +0100

    Better error handling

--- fix_db.py
+++ fix_db.py
@@ -7,6 +7,7 @@ from sfo.sfo import SfoFile as SfoFile
 import sqlite3
 import io
 import os
+import sys
 import argparse
 
 ## Argparse
@@ -20,16 +21,18 @@ PS5_IP = args.PS5_IP
 EXT_DISK = args.EXT_DISK
 ftp_port = 1337
 ps5_db_folder = '/system_data/priv/mms/'
+info_msg = 'INFO::'
+error_msg = 'ERROR::'
 files = []
 info = {}
 
 # Set variables based on parameters {ext0,ext1}
 if EXT_DISK == 'ext1':
     app_dir = '/mnt/ext1/user/app'
-    print(f"App directory set to {app_dir}")
+    print(f"{info_msg} App directory set to {app_dir}")
 elif EXT_DISK == 'ext0':
     app_dir = '/mnt/ext0/user/app'
-    print(f"App directory set to {app_dir}")
+    print(f"{info_msg} App directory set to {app_dir}")
 
 # CUSA class
 class CUSA :
@@ -74,25 +77,28 @@ ftp.login(user='username', passwd = 'password')
 # List of DB files to copy to temporary folder
 files_to_copy = ['app.db', 'appinfo.db']
 for file_name in files_to_copy:
-    print(f"INFO:: Copying file: {file_name} from {ps5_db_folder}")
+    print(f"{info_msg} Copying file: {file_name} from {ps5_db_folder}")
     try:
         ftp.cwd(ps5_db_folder)
         lf = open(dirs_to_create[0] + '/' + file_name, "wb")
         ftp.retrbinary(f"RETR {file_name}", lf.write)
         lf.close()
     except Exception as e:
-        print(f"ERROR:: Error copying {file_name}: {str(e)}")
+        print(f"{error_msg} Error copying {file_name}: {str(e)}")
 
 # Create list of games on the system that are (probably) missing
 ## If external, change to /mnt/ext0/user/app or /mnt/ext1/user/app
-if len(files) == 0:
-    ftp.cwd(app_dir)
-    ftp.dir(sort_files)
-    print(f"\nTitles found in {app_dir}:\n")
+ftp.cwd(app_dir)
+ftp.dir(sort_files)
+if len(files) > 0:
+    print(f"\n{info_msg} Titles found in {app_dir}:\n")
     for file in files:
         GameID = file.replace("'", "")
         cusa = get_game_info_by_id(GameID)
         print(cusa.sfo['TITLE_ID'] + ' - ' + cusa.sfo['TITLE'])
+else:
+    print(f"{error_msg} No titles found in {app_dir}")
+    sys.exit(1)
  
  ## Get current title id's from tbl_appinfo
 conn = sqlite3.connect(dirs_to_create[0] + '/' + "appinfo.db")
@@ -100,9 +106,9 @@ cursor = conn.cursor()
 cursor.execute("SELECT DISTINCT titleid from tbl_appinfo WHERE titleid like 'CUSA%%';")
 titles_appinfo = cursor.fetchall()
 if len(titles_appinfo) == 0:
-    print("\nNo CUSA titles found in database. Probably why you are running this script ;)")
+    print(f"\n{info_msg} No CUSA titles found in database. Probably why you are running this script ;)")
 else:
-    print("\nTitles found in database:")
+    print(f"\n{info_msg} Titles found in database:")
     for title in titles_appinfo:
         print(title[0])
 
@@ -115,15 +121,15 @@ missing_titles = files_set - titles_appinfo_set
 
 # Print the missing titles
 if len(missing_titles) > 0:
-    print(f"\nTitles in {app_dir} but not in the database:")
+    print(f"\n{info_msg} Titles in {app_dir} but not in the database:")
     for title in missing_titles:
         GameID = title.replace("'", "")
         cusa = get_game_info_by_id(GameID)
         print(cusa.sfo['TITLE_ID'] + ' - ' + cusa.sfo['TITLE'])
 elif len(missing_titles) == 0:
-    print(f"No titles missing in database which are also in {app_dir}")
+    print(f"{info_msg} No titles missing in database which are also in {app_dir}")
 else:
-    print(f"\nAll titles in {app_dir} are also in the database.")
+    print(f"\n{info_msg} All titles in {app_dir} are also in the database.")
 
 
 
